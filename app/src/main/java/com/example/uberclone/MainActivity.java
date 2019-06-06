03b6f026c5c644fab055efc6461442ad3ac4fad6
Delink <delinkdesigns@gmail.com>
2019-06-06 20:48:57 +0300

    install library butterknife a for views binding

--- app/src/main/java/com/example/uberclone/MainActivity.java
+++ app/src/main/java/com/example/uberclone/MainActivity.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import butterknife.BindView;
 import io.github.inflationx.calligraphy3.CalligraphyConfig;
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
 import io.github.inflationx.viewpump.ViewPump;
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_signIn) Button btnSignIn;
+    @BindView(R.id.btn_register) Button btnRegister;
 
     @Override
     protected void attachBaseContext(Context newBase) {
