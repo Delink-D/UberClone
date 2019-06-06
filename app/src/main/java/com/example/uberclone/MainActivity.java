bb8af0ba7867842c054dbb1eb2006709798c7713
Delink <delinkdesigns@gmail.com>
2019-06-06 19:52:20 +0300

    configure app to load assets on start of app

--- app/src/main/java/com/example/uberclone/MainActivity.java
+++ app/src/main/java/com/example/uberclone/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.uberclone;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import io.github.inflationx.calligraphy3.CalligraphyConfig;
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
+import io.github.inflationx.viewpump.ViewPump;
+import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ViewPump.init(ViewPump.builder()
+                .addInterceptor(new CalligraphyInterceptor(
+                        new CalligraphyConfig.Builder()
+                                .setDefaultFontPath("fonts/Arkhip_font.ttf")
+                                .setFontAttrId(R.attr.fontPath)
+                                .build()))
+                .build());
         setContentView(R.layout.activity_main);
     }
 }
